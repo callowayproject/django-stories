4a0f9a88e2f53b4eec878f8e5a90a9e815b383a6
Corey Oordt <coordt@washingtontimes.com>
2010-02-02 12:34:03 -0500

    Created a filter to add an attribute to a Tag class.

---
+++ stories/templatetags/add_attribute.py
@@ -0,0 +1,24 @@
+from django import template
+from BeautifulSoup import BeautifulSoup
+
+register = template.Library()
+
+@register.filter
+def add_attribute(tag, arg):
+    """
+    For paragraph pagination:
+
+    {% for obj in paginator.object_list %}
+        {% if forloop.first %}
+        {{ obj|add_attribute:"class=dropcap" }}
+        {% else %}
+        {{ obj }}
+        {% endif %}
+    {% endfor %}
+    """
+    try:
+        attr, val = arg.split('=')
+        tag[attr] = val
+    except Exception, e:
+        print "Exception!",e
+    return tag
