669e4e10361409dd75a02bcb4dbfa2992f81c467
Corey Oordt <coordt@washingtontimes.com>
2010-04-09 14:53:02 -0400

    Set the requirements to use our django-tinymce and set the official version to 0.3

--- stories/__init__.py
+++ stories/__init__.py
@@ -1,9 +1,9 @@
 __version_info__ = {
     'major': 0,
-    'minor': 2,
+    'minor': 3,
     'micro': 0,
-    'releaselevel': 'beta',
-    'serial': 5
+    'releaselevel': 'final',
+    'serial': 0
 }
 
 def get_version():
