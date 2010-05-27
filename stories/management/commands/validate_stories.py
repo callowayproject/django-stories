5326d6ff9596271892e6282c9b3db98075e6ed33
Justin Quick <justquick@gmail.com>
2010-05-27 02:06:32 -0400

    checks stories which have bad markup generated by smellington and does a tidy 4 u ;) + version bump

---
+++ stories/management/commands/validate_stories.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from django.core.exceptions import ImproperlyConfigured
+from stories.models import Story
+
+try:
+    from tidylib import tidy_fragment
+    from BeautifulSoup import BeautifulSoup, HTMLParseError
+except ImportError:
+    raise ImproperlyConfigured('You must install BeautifulSoup and pytidylib')
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+         for story in Story.objects.filter(pk=366949):
+            try:
+                BeautifulSoup(story.body)
+            except HTMLParseError, e:
+                story.body = tidy_fragment(story.body)
+                story.save()
