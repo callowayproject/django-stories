3caf1a7b0b18b80232b97d833dbf4eb983b787b4
Corey Oordt <coordt@washingtontimes.com>
2010-07-12 13:49:56 -0400

    bumping the version before I forget

--- stories/__init__.py
+++ stories/__init__.py
@@ -1,8 +1,8 @@
 __version_info__ = {
     'major': 0,
-    'minor': 3,
-    'micro': 14,
-    'releaselevel': 'final',
+    'minor': 4,
+    'micro': 0,
+    'releaselevel': 'beta',
     'serial': 1
 }
 
