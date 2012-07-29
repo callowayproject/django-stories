48e5a05f57a1a98a545e945dc49d8f5c936e5abb
Jose Soares <jose@linux.com>
2012-07-28 23:46:26 -0400

    More example app cleanup

--- example/simpleapp/admin.py
+++ example/simpleapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-from models import BasicThing
+from .models import BasicThing
+
 admin.site.register(BasicThing)
--- example/simpleprofile/admin.py
+++ example/simpleprofile/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-from models import Profile
+from .models import Profile
+
 admin.site.register(Profile)
