9ec3d172975d618b9d9ea1f291c02cdb6c8aa727
Jose Soares <jose@linux.com>
2012-08-28 22:27:55 -0400

    Changed the example settings_categories settings so the fields are setup via CATEGORIES_SETTINGS

--- example/settings_categories.py
+++ example/settings_categories.py
@@ -2,10 +2,16 @@ from example.settings import *
 
 INSTALLED_APPS += ('categories',)
 
-import categories
+CATEGORIES_SETTINGS = {
+    'FK_REGISTRY': {
+        'stories.story': {
+            'name': 'primary_category', 'related_name': 'primary_category'
+        }
+    },
+    'M2M_REGISTRY': {
+        'stories.story': {
+            'name': 'categories', 'related_name': 'categories'
+        }
+    }
+}
 
-from stories.models import Story
-categories.register_fk(
-    Story, 'primary_category', {'related_name': 'primary_category'})
-categories.register_m2m(
-    Story, 'categories', {'related_name': 'categories'})
