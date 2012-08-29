03d082af8f2488a3fd30c78e222ca62f3c2bad83
Jose Soares <jose@linux.com>
2012-08-29 18:02:18 -0400

    Removed the category speicfic settings and admin fieldset, this functionality should be handled by categories

--- stories/admin.py
+++ stories/admin.py
@@ -104,11 +104,6 @@ class StoryAdmin(AdminModel):
                        'status', 'origin', 'comment_status', )
         }),)
 
-    if settings.CATEGORY_FIELDS:
-        fieldsets += (_('Categories'), {
-            'fields': settings.CATEGORY_FIELDS}
-        )
-
     if settings.INCLUDE_PRINT:
         fieldsets = fieldsets + (_('Print Information'), {
             'fields': ('print_pub_date', 'print_section', 'print_page'),
