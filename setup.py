304e14aa1738b631d8d6614f5cee0ca4af817817
Corey Oordt <coreyoordt@gmail.com>
2013-03-27 05:04:50 -0500

    The register function of setup is choking on the README. commenting it out for now.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 import os
 from setuptools import setup, find_packages
 
+
 def read_file(filename):
     """Read a file into a string"""
     path = os.path.abspath(os.path.dirname(__file__))
@@ -13,9 +14,10 @@ def read_file(filename):
     except IOError:
         return ''
 
+
 def get_readme():
     """Return the README file contents. Supports text, rst, and markdown"""
-    for name in ('README','README.rst','README.md'):
+    for name in ('README', 'README.rst', 'README.md'):
         if os.path.exists(name):
             return read_file(name)
     return ''
@@ -23,18 +25,19 @@ def get_readme():
 # Use the docstring of the __init__ file to be the description
 DESC = " ".join(__import__('stories').__doc__.splitlines()).strip()
 
-setup(name='django-stories',
-      version= __import__('stories').get_version().replace(' ', '-'),
-      url='http://github.com/callowayproject/django-stories/',
-      author='Calloway Project',
-      author_email='webmaster@callowayproject.com',
-      description=DESC,
-      long_description=get_readme(),
-      packages=find_packages(),
-      include_package_data=True,
-      install_requires=read_file('requirements.txt'),
-      classifiers=[
-          'License :: OSI Approved :: Apache Software License',
-          'Framework :: Django',
-      ]
+setup(
+    name='django-stories',
+    version=__import__('stories').get_version().replace(' ', '-'),
+    url='http://github.com/callowayproject/django-stories/',
+    author='Calloway Project',
+    author_email='webmaster@callowayproject.com',
+    description=DESC,
+    # long_description=get_readme(),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_file('requirements.txt'),
+    classifiers=[
+      'License :: OSI Approved :: Apache Software License',
+      'Framework :: Django',
+    ]
 )
