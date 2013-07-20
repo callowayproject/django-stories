b9ccce33c6dd4145562d147453c9ae1fc231557a
Jose Soa <josefsoares@gmail.com>
2013-07-20 11:04:01 -0400

    [-] Removed the * import from the example urls

--- example/urls.py
+++ example/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, include
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
